0c522878a5af65b14e4297adf6425ecc34f22be4
Emily Wolsek <ewolsek@gmail.com>
2026-07-15 21:23:50 -0400

    started characters and location queries

--- db/queries/characters.js
+++ db/queries/characters.js
@@ -1,1 +1,17 @@
 import db from "#db/client";
+
+export async function getAllCharacters() {
+    const sql = `
+        SELECT * FROM characters;
+        `;
+        const {rows: characters} = await db.query(sql);
+    return characters;
+}
+
+export async function getCharacterById(id) {
+    const sql = `
+        SELECT * FROM characters WHERE id = $1;
+        `;
+        const {rows: [character]} = await db.query(sql, [id]);
+    return character;
+}
--- db/queries/locations.js
+++ db/queries/locations.js
@@ -1,2 +1,17 @@
 import db from "#db/client";
 
+export async function getAllLocations() {
+    const sql = `
+        SELECT * FROM locations;
+        `;
+    const {rows: locations} = await db.query(sql);
+return locations;
+}
+
+export async function getLocationById(id) {
+    const sql = `
+        SELECT * FROM locations WHERE id = $1;
+        `;
+    const {rows: [location]} = await db.query(sql, [id]);
+return location;
+}
