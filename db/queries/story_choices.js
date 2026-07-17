337ca4685d1f059b7605bc3d8a658f6f4c4c09bd
Emily Wolsek <ewolsek@gmail.com>
2026-07-16 21:54:40 -0400

    started story_choices query

--- db/queries/story_choices.js
+++ db/queries/story_choices.js
@@ -1,1 +1,27 @@
 import db from "#db/client";
+
+export async function createStoryChoice(
+    StoryNodeId,
+    label,
+    choiceText,
+    nextNodeId,
+    sortOrder,
+) {
+    const sql =`
+        INSERT INTO story_choices
+            (story_node_id, label, choice_text, next_node_id, sort_order)
+            VALUES
+            ($1, $2, $3, $4, $5)
+        `;
+    const { rows: [storyChoice],
+    } = await db.query(sql, [StoryNodeId, label, choiceText, nextNodeId, sortOrder]);
+return storyChoice;
+}
+
+export async function getAllStoryChoices() {
+    const sql = `
+        SELECT * FROM story_choices
+        `;
+    const { rows: storyChoices } = await db.query(sql);
+return storyChoices;
+}
