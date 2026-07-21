937bffacc873dc99acf3c3b8e68042b6edb53000
Qyce Broussard <qbroussard18@gmail.com>
2026-07-20 19:03:44 -0500

    Completed characters router and started working on the queries and routers for user inventory

--- db/queries/characters.js
+++ db/queries/characters.js
@@ -1,27 +1,36 @@
 import db from "#db/client";
 
-export async function createCharacter(name, animal_type, description) {
-    const sql = `
-        INSERT INTO characters (name, animal_type, description)
-        VALUES ($1, $2, $3)
+export async function createCharacter(
+  name,
+  animal_type,
+  description,
+  imageUrl,
+) {
+  const sql = `
+        INSERT INTO characters (name, animal_type, description, image_url)
+        VALUES ($1, $2, $3, $4)
         RETURNING *;
         `;
-        const {rows:[characters]} = await db.query(sql, [name, animal_type, description]);
-    return characters;
+  const {
+    rows: [character],
+  } = await db.query(sql, [name, animal_type, description, imageUrl]);
+  return character;
 }
 
 export async function getAllCharacters() {
-    const sql = `
+  const sql = `
         SELECT * FROM characters;
         `;
-        const {rows: characters} = await db.query(sql);
-    return characters;
+  const { rows: characters } = await db.query(sql);
+  return characters;
 }
 
 export async function getCharacterById(id) {
-    const sql = `
+  const sql = `
         SELECT * FROM characters WHERE id = $1;
         `;
-        const {rows: [character]} = await db.query(sql, [id]);
-    return character;
+  const {
+    rows: [character],
+  } = await db.query(sql, [id]);
+  return character;
 }
