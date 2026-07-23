bcfff156c9254cf97cf9dc3ba09e7c40048c0d7c
Qyce Broussard <qbroussard18@gmail.com>
2026-07-23 18:10:16 -0500

    updated image links for characters

--- db/queries/characters.js
+++ db/queries/characters.js
@@ -7,7 +7,7 @@ export const characters = [
     description:
       " A small yet mighty adventurer who doesn't back down from a fight, however his aim when catching prey could use some work",
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1526381949013196920/37210721-F4C9-4BC3-AF43-253F7D6E7D1E.png?ex=6a56d177&is=6a557ff7&hm=85521e0d88811c791f494e63474dddd81cc23f32770dff2e06b9447f04aac5a5&",
+      "https://drive.google.com/file/d/1bftNpf70FfNO1f_iyklIZrrRZs1-vv7J/view?usp=sharing",
   },
   {
     name: "Athena",
@@ -15,7 +15,7 @@ export const characters = [
     description:
       "A fur missile with limitless energy, a sharp mind, and a knack for finding trouble, she wont stop until she completes whatever task is set before her.",
     imageUrl:
-      "https://media.discordapp.net/attachments/1523867510359720010/1526952398076252212/05D812C0-54F9-4056-8032-12EF1AE73DAF.png?ex=6a621f3d&is=6a60cdbd&hm=c0d7dc3a42c1bf8ef2c0e589bd2780e709fb7a92dcf231956033134f193e3331&=&format=webp&quality=lossless&width=890&height=890",
+      "https://drive.google.com/file/d/1ukUMYc0Y0alQtdlWLl13egNUg5DX0G0v/view?usp=sharing",
   },
   {
     name: "Tray",
@@ -23,7 +23,7 @@ export const characters = [
     description:
       "A loyal, intelligent and protective companion with playful spirit. Always ready for the next adventure.",
     imageUrl:
-      "https://media.discordapp.net/attachments/1523867510359720010/1526952446759534797/295A859A-F2E7-4DC2-89C6-6774B57F5151.png?ex=6a621f48&is=6a60cdc8&hm=afdfd23fc9cda7640b64f49a18bd9d2c6163a524a04eea0a4576daad17e0742a&=&format=webp&quality=lossless&width=890&height=890",
+      "https://drive.google.com/file/d/1GaaqupMAtiUv6RHJgThs0KkXu2_2w_94/view?usp=sharing",
   },
   {
     name: "Opie",
@@ -31,7 +31,7 @@ export const characters = [
     description:
       "As affectionate as he is clever, this tuxedo balances a brilliant and calculating mind with a deeply loving heart. Always figuring out the smartest way to handle trouble, but just as quick to cuddle up when the dust settles!",
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1526381991862472704/1225AD3D-7550-4EEE-8ADC-866AE00EF7DC.png?ex=6a620641&is=6a60b4c1&hm=919db7ddd0129dc7d46956e9fd277eabb444920bb8b707da14a078d89c9ea58a",
+      "https://drive.google.com/file/d/1GGzN-grTVnrdAMTb5oPNHJE-9GSyrYSf/view?usp=sharing",
   },
   {
     name: "Ajax",
@@ -39,7 +39,7 @@ export const characters = [
     description:
       "A wild little spitfire with boundless energy. He's a little stinker, but he'll steal your heart!",
     imageUrl:
-      "https://media.discordapp.net/attachments/1523867510359720010/1524943777134018631/Ajax_asset.png?ex=6a6210d0&is=6a60bf50&hm=c13701aea2c0b8565ba7dbf6c89ce08974c633d029d6f49b80ffa799900ecbf4&=&format=webp&quality=lossless&width=890&height=890",
+      "https://drive.google.com/file/d/1IqnGUMki8WyczNZm5hG8S3_p4VU8F68u/view?usp=sharing",
   },
   {
     name: "Lincoln",
@@ -47,7 +47,7 @@ export const characters = [
     description:
       "A total wildcard, Lincoln excels at making a class about web development feel fresh and exciting with every appearance.",
     imageUrl:
-      "https://media.discordapp.net/attachments/1523867510359720010/1528940102078824568/8E5A8CC3-D1F7-499B-8AAE-D63A44975F74.png?ex=6a621a2e&is=6a60c8ae&hm=c516a6d0f0718d95c6c28f756f93d1a12c76be3544e16b9d3b7f2fd5d56ff23f&=&format=webp&quality=lossless&width=890&height=890",
+      "https://drive.google.com/file/d/1QSSDKwFdgTHJKzCqZmhh6CgtOzisCs98/view?usp=sharing",
   },
 ];
 
