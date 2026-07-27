66088cbfdc5b0972b69ba9e76890aa72de8b51e6
Qyce Broussard <qbroussard18@gmail.com>
2026-07-27 18:17:32 -0500

    updated image links for enemies and bosses

--- db/queries/bosses.js
+++ db/queries/bosses.js
@@ -5,21 +5,21 @@ export const bosses = [
     name: "Rabid Squirrel",
     hp: 5,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529637477260726352/rabid_squirrel.png?ex=6a62a969&is=6a6157e9&hm=8bd5a0913f11c5f55c956ed38feab2c7034a121067b9e5590ab31a4612aa0d6f",
+      "https://drive.google.com/file/d/1i50XN90J3GOXAxVFGSUbraGNfyULt-v1/view?usp=sharing",
     locationId: 1,
   },
   {
     name: "Rival Stray",
     hp: 5,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529638267954401412/Rival_stray.png?ex=6a62aa26&is=6a6158a6&hm=80d6dd5dd7c3fbcfc3baa6aa071d9de021dd1d25412f526845c928e5586f5e01",
+      "https://drive.google.com/file/d/1c3L72JsooSQBnybT-VXLnZmnhfu789wd/view?usp=sharing",
     locationId: 2,
   },
   {
     name: "Animal Control",
     hp: 5,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529641243494322186/animal_control.png?ex=6a62aceb&is=6a615b6b&hm=bfecd9cad0b6e2ba1c385440acbf0791a376c5fb1b02e898556dae866b0b2a5b",
+      "https://drive.google.com/file/d/13CPgMuR9QIn0xtBEtPYHNjRaKK29Okg-/view?usp=sharing",
     locationId: 3,
   },
 ];
--- db/queries/enemies.js
+++ db/queries/enemies.js
@@ -5,63 +5,63 @@ export const enemies = [
     name: "Rabbit",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529645770784116746/rabbit.png?ex=6a62b123&is=6a615fa3&hm=3c60b304782df0a01e046ebf6ec5e206210fa2e63197877d7fc8db61d88cb63b",
+      "https://drive.google.com/file/d/1DoYMahDkMJl2KNBXKx1Qcb6pT3d-K7o8/view?usp=sharing",
     locationId: 1,
   },
   {
     name: "Squirrel",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529645859384459274/squirrel.png?ex=6a62b138&is=6a615fb8&hm=89acf3cef8b9884c4cb7e7e14f25d100de45ed698e39d2fb5585a67bd21f9411",
+      "https://drive.google.com/file/d/1twR7bHsgn3vVsMQ2Ls58SCteDvBAoS1b/view?usp=sharing",
     locationId: 1,
   },
   {
     name: "Fox",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529646657388675183/fox.png?ex=6a62b1f6&is=6a616076&hm=9b6a3bdd9950d3821cd738f803c3e9d2732fc85fcf1315ea92c4512c3af17cb6",
+      "https://drive.google.com/file/d/1DoYMahDkMJl2KNBXKx1Qcb6pT3d-K7o8/view?usp=sharing",
     locationId: 1,
   },
   {
     name: "Armadillo",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529647458643214396/Armadillo.png?ex=6a62b2b5&is=6a616135&hm=b616333f48084daeda6d77796d1bb7dbba27bcfabf911efca85aada2fa24feb2",
+      "https://drive.google.com/file/d/1XKC0csyHcVTnY8iQdEQg_8w83MJsDmcQ/view?usp=sharing",
     locationId: 2,
   },
   {
     name: "Snake",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529649442754400266/snake.png?ex=6a62b48e&is=6a61630e&hm=1372a70048c0f047d2a067a7b783e2f1d0b71cc21d26afdb15386011228dca75",
+      "https://drive.google.com/file/d/1W37gswgJg6iBAU57x3qB_qe6q7aDFsv_/view?usp=sharing",
     locationId: 2,
   },
   {
     name: "Hawk",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529650072806096997/hawk.png?ex=6a62b524&is=6a6163a4&hm=c92c243b5db01dfe790b9c8c3065751ee9b6805f642a8c50157b358cc34c1cd6",
+      "https://drive.google.com/file/d/1aTKN7SvFrQUxspoWNy77FU9YCAZ2B_3W/view?usp=sharing",
     locationId: 2,
   },
   {
     name: "Stray Animal",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529651584554897659/stray_animal.png?ex=6a62b68d&is=6a61650d&hm=1a62282f83256272153b38ae45818f4990281ac7cfb4a098d2ffe66ffccd5b1a",
+      "https://drive.google.com/file/d/1IhSEKmFPVmUNKEZvfjqZoQ93btmFJVXA/view?usp=sharing",
     locationId: 3,
   },
   {
     name: "Rat",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529652482861568240/rat.png?ex=6a62b763&is=6a6165e3&hm=82973d349253c8068fa4209f530f13a59a0f806f30ba685f33fd388bae5d7ec6",
+      "https://drive.google.com/file/d/1YVUPmpamGXifX-KAJPYIlrttmI9xQhv-/view?usp=sharing",
     locationId: 3,
   },
   {
     name: "Aggressive Kid",
     hp: 3,
     imageUrl:
-      "https://cdn.discordapp.com/attachments/1523867510359720010/1529653799352795176/aggressive_kid.png?ex=6a62b89d&is=6a61671d&hm=cfe5ccc87bc453ffeff0afb59a9742b2bfd850bd4675ef2cdeebece0087016d3",
+      "https://drive.google.com/file/d/1BcUALURFcieyxTE9ryUTwDfNC9IltVfU/view?usp=sharing",
     locationId: 3,
   },
 ];
